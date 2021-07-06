01ae64604886ade9a6e56b481050833c25fc315e
东 <18855238097@163.com>
2021-07-06 15:10:51 +0800

    notFound 插件

--- config/plugin.js
+++ config/plugin.js
@@ -1,9 +1,7 @@
 'use strict';
+const path = require('path');
 
-/** @type Egg.EggPlugin */
-module.exports = {
-  // had enabled by egg
-  // static: {
-  //   enable: true,
-  // }
+exports.notFound = {
+  enable: true,
+  path: path.join(__dirname, '../lib/plugin/egg-notFound'),
 };
